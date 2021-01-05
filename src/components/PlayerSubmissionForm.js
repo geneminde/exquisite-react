80809408513f5810769cfa1d187928c1e9865327
Genevieve Neely <genevieve.hood@gmail.com>
2021-01-05 12:27:32 -0800

    setup app and add player state to PlayerSubmissionForm

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+  const [player, setPlayer] = useState(1)
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{player}</h3>
 
       <form className="PlayerSubmissionForm__form" >
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
           {
-            // Put your form inputs here... We've put in one below as an example
+           
           }
           <input
             placeholder="hm..."
             type="text" />
+          <input placeholder="another placeholder" type="text"/>
 
         </div>
 
